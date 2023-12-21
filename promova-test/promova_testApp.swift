2cd1435a9c8f8091a218b7d977f357ecf6d914d1
Bogdan Sarkisov <b.sarkisov@mobidev.biz>
2023-12-21 14:24:07 +0200

    Initial Commit

---
+++ promova-test/promova_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  promova_testApp.swift
+//  promova-test
+//
+//  Created by User on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct promova_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
